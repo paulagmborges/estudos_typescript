649e60a528a3273b79bc84aaad06f5c0e16d1d55
paula borges <paulabzambeli@gmail.com>
2024-08-02 10:40:19 -0300

    feat(dependencies): add async, express, and cors libraries
    Adicionadas as seguintes bibliotecas ao projeto: - "express-async-error": Middleware para tratamento
    de erros assíncronos em Express, "cors":Middleware para permitir requisições de diferentes origens

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
 import express, { Request, Response, NextFunction } from 'express';
 import router from './routes.ts/routes'; 
-
+import "express-asunc-errors"
 const app = express();
 const port = 3000;
 
 app.use(express.json()); 
 app.use(router);
+// Middleware de tratamento de erros
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return res.status(400).json({
+            error: err.message
+        });
+    }
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error'
+    });
+});
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
