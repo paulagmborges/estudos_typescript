77afb97173932493c382d052ab70ba5d0674265a
paula borges <paulabzambeli@gmail.com>
2024-08-03 11:09:45 -0300

    correcao de importacao no controller de dlate de categories

--- src/controllers/category/DeleteCategoryController.ts
+++ src/controllers/category/DeleteCategoryController.ts
@@ -1,3 +1,4 @@
+import { DeleteCategoryRequest } from './../../models/interfaces/category/DeleteRequest';
 import { Request, Response } from "express";
 import { DeleteCategoryService } from "../../service/category/DeleteCategoryService";
 
@@ -5,7 +6,7 @@ class DeleteCategoryController {
   async handle(req: Request, res: Response) {
     const category_id  = req.query.category_id as string
     const deleteCategoryService = new DeleteCategoryService();
-    const result = await deleteCategoryService.execute(category_id);
+    const result = await deleteCategoryService.execute({ category_id } as DeleteCategoryRequest);
       return res.json(result);
   }}
   export {DeleteCategoryController}
